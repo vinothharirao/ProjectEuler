61510175cfa30534e7457e1b751e0db0068fc799
Vinoth H <Vinoth H>
2016-03-14 10:07:37 +0530

    first commit

--- 006.js
+++ 006.js
@@ -1,5 +1,9 @@
+//Solution to find difference between square of sum of first 100 naturals and sum of their squares.
+
 var limit = 100;
-var sumOfSquares = (limit)*(limit+1)*(2*limit+1)/6
+//sum of squares of n natural numbers = (n)(n+1)(2n+1)/6
+var sumOfSquares = (limit)*(limit+1)*(2*limit+1)/6;
+//sum of n natural numbers = (n)(n+1)/2
 var squareOfSum = (limit)*(limit+1)/2;
 squareOfSum = squareOfSum*squareOfSum;
 
