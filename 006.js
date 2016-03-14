b8bf057e5a2b753bdc15793b354060bc750f3e2d
Vinoth H <Vinoth H>
2016-03-14 10:05:16 +0530

    first commit

--- 006.js
+++ 006.js
@@ -1,10 +1,6 @@
-console.time("time");
-
 var limit = 100;
 var sumOfSquares = (limit)*(limit+1)*(2*limit+1)/6
 var squareOfSum = (limit)*(limit+1)/2;
 squareOfSum = squareOfSum*squareOfSum;
 
 console.log(squareOfSum - sumOfSquares);
-
-console.timeEnd("time");
