9c03975543fc3fdabf045e3759fdb45edd656a23
unknown <hvinoth18@gmail.com>
2016-03-19 17:15:05 +0530

    initial commit

---
+++ 022.js
@@ -0,0 +1,23 @@
+var fs = require('fs');
+var names = fs.readFileSync('p022_names.txt').toString().split(",");
+
+names.sort();
+console.log(totalScore(names));
+
+//sum all scores.
+function totalScore(names) {
+	var total = 0;
+	for(var i = 0;i<names.length;i++) {
+		total += score(names[i],i+1);
+	}
+	return total;
+}
+//find score of a given string.
+function score(name,rank) {
+	var sum = 0;
+	//discard start of and end of string characters.
+	for(var i=1;i<name.length-1;i++) {
+		sum += (name[i].charCodeAt(0))-64;
+	}
+	return sum * rank;
+}
