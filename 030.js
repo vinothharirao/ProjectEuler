52b3232c22c5d15ce6b8fb58fb8c2899462e6da2
Vinoth H <hvinoth18@gmail.com>
2016-03-25 22:28:21 +0530

    initial-commit

---
+++ 030.js
@@ -0,0 +1,33 @@
+var sum = 0;
+var number = 2;
+
+var upperBound = calculateBound(5);
+
+while(true) {
+	if(isValid(number)) sum += number;
+	number++;
+	if(number == upperBound) break;
+}
+console.log(sum);
+
+function calculateBound(power) {
+	var noOfdigits = 1;
+	largest = 9;
+	while(Math.pow(9,power) * noOfdigits >= largest) {	
+		noOfdigits ++;
+		largest = (largest * 10) + 9;
+	}
+	return largest;
+}
+function isValid(n) {
+	if(n == SumOfPowers(n,5)) return true;
+	return false;
+}
+function SumOfPowers(n,pow) {
+	var sum = 0;
+	while(n) {
+		sum += Math.pow(n%10,pow);
+		n = Math.floor(n/10);
+	}
+	return sum;
+}
