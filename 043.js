c7f25b3d82c18519faa41df816e1c8201c54a007
Vinoth H <hvinoth18@gmail.com>
2016-03-26 13:32:25 +0530

    initial-commit

---
+++ 043.js
@@ -0,0 +1,44 @@
+var permArr = [],
+  usedChars = [];
+var sum = 0;
+
+var digits = [];
+var primes = [2,3,5,7,11,13,17];
+for(var i=0;i<10;i++) digits[i] = i;
+var result = permute(digits);
+console.log(result);
+
+function sumUp(current,permutation) {
+  var number = 0;
+  for(var i = permutation.length-1;i>=0;i--) {
+    number += permutation[i] * Math.pow(10,permutation.length-1-i);    
+  }
+  return current + number;
+}
+function checkDivisible(permutation) {
+  var pointer = 4;
+  if( (permutation[3] % 2 != 0) || (permutation[5] % 5 != 0) ) return false;
+  if( (permutation[2]+permutation[3]+permutation[4])%3 != 0) return false;
+  while(pointer < 8) {
+    if( !((permutation[pointer]*100 + permutation[pointer+1]*10 + permutation[pointer+2]) % primes[pointer-1] == 0) ) return false;
+    pointer ++;
+  }
+  return true;
+}
+function permute(input) {
+  var i, ch;
+  for (i = 0; i < input.length; i++) {
+    ch = input.splice(i, 1)[0];
+    usedChars.push(ch);
+    if (input.length == 0) {
+      var item = usedChars.slice();
+      if(checkDivisible(item)) {
+        sum = sumUp(sum,item);        
+      }      
+    }
+    permute(input);
+    input.splice(i, 0, ch);
+    usedChars.pop();
+  }  
+  return sum;
+};
